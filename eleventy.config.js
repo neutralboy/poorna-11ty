35300f2f906722c9da61700c6fee29f36577fac3
neutralboy <leopoorna99@gmail.com>
2023-05-24 23:25:09 +0530

    added time stamping

--- eleventy.config.js
+++ eleventy.config.js
@@ -16,6 +16,11 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ "public": "public" });
 
+  eleventyConfig.addFilter("toLocaleString", function(date) {
+    const dateString = new Date(date);
+    const options = { month: 'long', year: 'numeric' };
+    return dateString.toLocaleDateString('en-US', options);
+  });
 
   eleventyConfig.addCollection("blogPosts", (api) => {
     console.log(api.getAll().filter(p => p.data.layout === "post.html"))
---
+++ post/the-arc-of-healthcare.md
@@ -0,0 +1,13 @@
+---
+title: The Arc of Healthcare
+date: 2023-05-23 20:59:39
+keywords: Healthcare, Medicine, Doctor
+description: Where is the field of healthcare of going towards to ? What does AI mean for healthcare ?
+level: Opinion
+layout: post.html
+image: https://poorna-657a.s3.ap-south-1.amazonaws.com/Screenshot+from+2022-10-18+18-44-04.png
+---
+
+```
+Work in Progress
+```
